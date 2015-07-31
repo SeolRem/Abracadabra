f89c1026c40e8e9b5257753f1f58f1c1b8ce339a
seola <seolakim93@gmail.com>
2015-07-31 20:17:08 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Abracadabra::Application.config.secret_key_base = 'c7b28f95b7206a0ec735ac26cda09d5b7137e7d70751d6a884dcebf44fa9b5a6fa4d81a9a14ba9b6aad761163276ff59ec57410de9c5f7676e0a110e1300466d'
---
+++ db/migrate/20150728150609_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :login
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password
+      t.datetime :last_login
+
+      t.timestamps
+    end
+  end
+end
